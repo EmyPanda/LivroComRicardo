85502afc7cb426d46cb3669dad99d35f9d3b9efb
EmyPanda <emyllef123@gmail.com>
2019-08-15 11:42:53 -0300

    primeiro commit

---
+++ src/livro.java
@@ -0,0 +1,14 @@
+
+public class livro {
+    private
+    
+    String Titulo;
+    String Autor;
+    String ISBN;
+    String Editora;
+    int ano;
+    int qtd_paginas;
+    int valor;
+    
+    
+}
